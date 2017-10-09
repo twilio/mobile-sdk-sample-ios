74ce0a70fed6e37d81bed28f0cf4e157a3e1b63b
Edgars Avotins <edgars.avotins@testdevlab.com>
2017-10-09 23:48:57 +0300

    Fixed Perl script

--- perl_script.sh
+++ perl_script.sh
@@ -1,8 +1,9 @@
 perl -pi -w -e "s/9EVH78F4V4/G3988GSF98/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
-perl -pi -w -e "s/iPhone Developer: Adriana Pineda (7AQUE3U49X)/iPhone Developer/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
+perl -pi -w -e "s/iPhone Developer: Adriana Pineda \(7AQUE3U49X\)/iPhone Developer/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
 perl -pi -w -e "s/com.twilio.authenticator.sample/com.twilio.authenticator.sample.TestDevLab/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
 perl -pi -w -e "s/c4191781-7654-4a59-81b0-7c5f5f567397/1f51a4f9-17cf-46f5-93f9-a05f6764d800/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
-perl -pi -w -e "s/"TwilioAuthenticator Sample Dev Profile"/AuthySampleDProfile/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
-perl -pi -w -e "s/iPhone Distribution: Authy Inc. (9EVH78F4V4)/iPhone Developer/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
+perl -pi -w -e 's/"TwilioAuthenticator Sample Dev Profile"/AuthySampleDProfile/g;' TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
+perl -pi -w -e "s/iPhone Distribution: Authy Inc. \(9EVH78F4V4\)/iPhone Developer/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
+perl -pi -w -e "s/iPhone Distribution: Authy Inc. \(G3988GSF98\)/iPhone Developer/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
 perl -pi -w -e "s/3f9e1946-6608-4e49-ba10-26de21c2721c/1f51a4f9-17cf-46f5-93f9-a05f6764d800/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj &&
 perl -pi -w -e "s/TwilioAuthenticator Sample Prod Profile/AuthySampleDProfile/g;" TwilioAuthenticatorSample.xcodeproj/project.pbxproj
