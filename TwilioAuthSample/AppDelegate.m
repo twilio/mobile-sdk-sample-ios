d18b818bfabd90f8a8b53db7718bf54f9ab7bc6e
Adriana Pineda <apineda@twilio.com>
2019-03-15 15:07:06 -0500

    Support removal of push notifications

--- TwilioAuthSample/AppDelegate.m
+++ TwilioAuthSample/AppDelegate.m
@@ -11,6 +11,15 @@
 
 #import "ApprovalRequestsViewController.h"
 #import "RequestDetailViewController.h"
+#import <UserNotifications/UserNotifications.h>
+
+#define PUSH_NOTIFICATION_ONETOUCH_CATEGORY_IDENTIFIER @"onetouch_approval_request"
+#define PUSH_NOTIFICATION_ONETOUCH_APPROVE_OPTION @"PUSH_NOTIFICATION_ONETOUCH_APPROVE"
+#define PUSH_NOTIFICATION_ONETOUCH_DENY_OPTION @"PUSH_NOTIFICATION_ONETOUCH_DENY"
+#define PUSH_NOTIFICATION_ONETOUCH_APPROVE_TEXT @"Approve"
+#define PUSH_NOTIFICATION_ONETOUCH_DENY_TEXT @"Deny"
+#define ONE_TOUCH_PUSH_NOTIFICATION_APPROVAL_REQUEST_UUID @"approval_request_uuid"
+#define PUSH_NOTIFICATION_TYPE_KEY @"type"
 
 @implementation AppDelegate
 
@@ -63,13 +72,6 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
 }
 
-- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
-
-    if (notificationSettings != UIUserNotificationTypeNone) {
-        [application registerForRemoteNotifications];
-    }
-}
-
 - (void)configureRootController {
     UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
 
@@ -78,6 +80,87 @@
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
     [self.window setRootViewController:navigationController];
     [self.window makeKeyAndVisible];
+}
+
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(nonnull NSDictionary *)userInfo completionHandler:(nonnull void (^)())completionHandler {
+    [self updateOneTouchRequestFromNotificationWithStatus:identifier userInfo:userInfo completionHandler:completionHandler];
+
+}
+
+
+- (void)updateOneTouchRequestFromNotificationWithStatus:(NSString *)statusOption userInfo:(NSDictionary *)userInfo completionHandler:(void (^)())completionHandler {
+
+    if (![self isOneTouchPushNotificationRequest:userInfo]) {
+        return;
+    }
+
+    AUTApprovalRequestStatus status;
+    if ([statusOption isEqualToString:PUSH_NOTIFICATION_ONETOUCH_APPROVE_OPTION]) {
+        status = AUTApprovalRequestStatusApproved;
+    } else if ([statusOption isEqualToString:PUSH_NOTIFICATION_ONETOUCH_DENY_OPTION]) {
+        status = AUTApprovalRequestStatusDenied;
+    } else {
+        return;
+    }
+
+    NSString *approvalRequestUUID = [userInfo objectForKey:ONE_TOUCH_PUSH_NOTIFICATION_APPROVAL_REQUEST_UUID];
+    if (approvalRequestUUID == nil || [approvalRequestUUID isEqualToString:@""]) {
+        return;
+    }
+
+    TwilioAuthConfiguration *config = [TwilioAuthConfiguration configurationWithUserDefaultsGroup:@"group.twilio.auth.sample.12345"];
+    [TwilioAuth setupWithConfiguration:config];
+    TwilioAuth *sharedAuth = [TwilioAuth sharedInstance];
+    [sharedAuth getRequestWithUUID:approvalRequestUUID completion:^(AUTApprovalRequest *request, NSError *error) {
+
+        if (status == AUTApprovalRequestStatusApproved) {
+            [self approveRequest:request completionHandler:completionHandler];
+        } else {
+            [self denyRequest:request completionHandler:completionHandler];
+        }
+
+    }];
+
+}
+
+- (void)approveRequest:(AUTApprovalRequest *)request completionHandler:(void (^)())completionHandler {
+
+    TwilioAuthConfiguration *config = [TwilioAuthConfiguration configurationWithUserDefaultsGroup:@"group.twilio.auth.sample.12345"];
+    [TwilioAuth setupWithConfiguration:config];
+    TwilioAuth *sharedAuth = [TwilioAuth sharedInstance];
+    [sharedAuth approveRequest:request completion:^(NSError *error) {
+
+        if (completionHandler) {
+            completionHandler();
+        }
+
+    }];
+
+}
+
+- (void)denyRequest:(AUTApprovalRequest *)request completionHandler:(void (^)())completionHandler {
+
+    TwilioAuthConfiguration *config = [TwilioAuthConfiguration configurationWithUserDefaultsGroup:@"group.twilio.auth.sample.12345"];
+    [TwilioAuth setupWithConfiguration:config];
+    TwilioAuth *sharedAuth = [TwilioAuth sharedInstance];
+    [sharedAuth denyRequest:request completion:^(NSError *error) {
+
+        if (completionHandler) {
+            completionHandler();
+        }
+
+    }];
+
+}
+
+- (BOOL)isOneTouchPushNotificationRequest:(NSDictionary *)userInfo {
+
+    NSString *notificationType = [userInfo objectForKey:PUSH_NOTIFICATION_TYPE_KEY];
+    if ([notificationType isEqualToString:PUSH_NOTIFICATION_ONETOUCH_CATEGORY_IDENTIFIER]) {
+        return YES;
+    }
+
+    return NO;
 }
 
 #pragma mark - Register for Push Notifications
@@ -101,6 +184,8 @@
     }
 
     // Check if device has been registered
+    TwilioAuthConfiguration *config = [TwilioAuthConfiguration configurationWithUserDefaultsGroup:@"group.twilio.auth.sample.12345"];
+    [TwilioAuth setupWithConfiguration:config];
     TwilioAuth *sharedTwilioAuth = [TwilioAuth sharedInstance];
     if (![sharedTwilioAuth isDeviceRegistered]) {
         return;
@@ -131,10 +216,69 @@
 }
 
 - (void)registerForPushNotifications:(UIApplication *)application {
+    [self registerForRemoteNotificationsWithActionsForApplication:application];
+}
 
-    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert) categories:nil];
+- (UNNotificationAction *)getApproveActionForNotificationsForiOS8AndAbove {
 
-    [application registerUserNotificationSettings:settings];
+    UNNotificationAction *approveAction = [UNNotificationAction actionWithIdentifier:PUSH_NOTIFICATION_ONETOUCH_APPROVE_OPTION title:PUSH_NOTIFICATION_ONETOUCH_APPROVE_TEXT options:UNNotificationActionOptionAuthenticationRequired];
+
+    return approveAction;
+}
+
+- (UNNotificationAction *)getDenyActionForNotificationsForiOS8AndAbove {
+
+    UNNotificationAction *denyAction = [UNNotificationAction actionWithIdentifier:PUSH_NOTIFICATION_ONETOUCH_DENY_OPTION title:PUSH_NOTIFICATION_ONETOUCH_DENY_TEXT options:UNNotificationActionOptionDestructive | UNNotificationActionOptionAuthenticationRequired];
+
+    return denyAction;
+
+}
+
+- (UNNotificationCategory *)getOneTouchCategoryForiOS8AndAboveWithActions:(NSArray *)actions {
+
+    UNNotificationCategory *actionCategory = [UNNotificationCategory categoryWithIdentifier:PUSH_NOTIFICATION_ONETOUCH_CATEGORY_IDENTIFIER actions:actions intentIdentifiers:@[] options:UNNotificationCategoryOptionCustomDismissAction];
+    return actionCategory;
+}
+
+- (UNNotificationCategory *)getOneTouchCategoryForiOS8AndAbove {
+
+    UNNotificationAction *approveAction = [self getApproveActionForNotificationsForiOS8AndAbove];
+
+    UNNotificationAction *denyAction = [self getDenyActionForNotificationsForiOS8AndAbove];
+
+    UNNotificationCategory *actionCategory = [self getOneTouchCategoryForiOS8AndAboveWithActions:@[approveAction, denyAction]];
+
+    return actionCategory;
+}
+
+- (void)registerForRemoteNotificationsWithActionsForApplication:(UIApplication *)application {
+
+    /** Sample code for onetouch push notification payload
+     {
+     alert = "OneTouch Testing just sent you a new approval request";
+     "approval_request_uuid" = "407802b0-3e4e-0135-ee46-06ca50569adc";
+     aps =     {
+     alert = "OneTouch Testing just sent you a new approval request";
+     badge = 1;
+     category = "onetouch_approval_request";
+     sound = default;
+     };
+     "serial_id" = 1053915;
+     "twi_message_id" = APN54d05980bbf64376a0b402de620f2b96;
+     type = "onetouch_approval_request";
+     }
+     */
+
+    UNNotificationCategory *actionCategory = [self getOneTouchCategoryForiOS8AndAbove];
+    NSSet *categories = [NSSet setWithObject:actionCategory];
+
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    [center setNotificationCategories:categories];
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    }];
+
+    [application registerForRemoteNotifications];
+
 }
 
 #pragma mark - Handle Push Notification
@@ -190,6 +334,8 @@
     }
 
     NSString *approvalRequestUUID = [userInfo objectForKey:@"approval_request_uuid"];
+    TwilioAuthConfiguration *config = [TwilioAuthConfiguration configurationWithUserDefaultsGroup:@"group.twilio.auth.sample.12345"];
+    [TwilioAuth setupWithConfiguration:config];
     TwilioAuth *twilioAuth = [TwilioAuth sharedInstance];
     [twilioAuth getRequestWithUUID:approvalRequestUUID completion:^(AUTApprovalRequest *request, NSError *error) {
 
@@ -210,4 +356,29 @@
     [self handlePushNotificationWithInfo:userInfo];
 }
 
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+
+    /**
+     Sample payload of `remove_push` notification
+     {"aps":{"content-available":1, "sound":""}, "type":"remove_push"}
+     */
+    if ([[userInfo objectForKey:@"type"] isEqualToString:@"remove_push"]) {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+
+        [center getDeliveredNotificationsWithCompletionHandler:^(NSArray<UNNotification *> *notifications) {
+
+            NSMutableArray *identifiersToRemove = [[NSMutableArray alloc] init];
+
+            for (UNNotification *notification in notifications) {
+                if ([notification.request.content.categoryIdentifier isEqualToString:PUSH_NOTIFICATION_ONETOUCH_CATEGORY_IDENTIFIER]) {
+                    [identifiersToRemove addObject:notification.request.identifier];
+                }
+            }
+
+            [center removeDeliveredNotificationsWithIdentifiers:identifiersToRemove];
+        }];
+    }
+
+    completionHandler(UIBackgroundFetchResultNewData);
+}
 @end
